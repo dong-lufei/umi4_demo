ba2855ab6c8ccb8fc3b3071218bb1db680877be8
dlf <136975643@qq.com>
2022-07-03 14:30:11 +0800

    f

---
+++ README.md
@@ -0,0 +1,5 @@
+# env —— 配置 DATABASE_URL、UPSTASH_REDIS_REST_URL 和 JWT_SECRET
+
+# 安装依赖 —— pnpm i
+
+# 启动 —— pnpm dev
---
+++ src/components/TextInput.tsx
@@ -0,0 +1,32 @@
+import React from "react"
+
+function TextInput(props: {
+  value: string
+  type?: string
+  onChange(v: string): void
+  textArea?: boolean
+}) {
+  if (props.textArea) {
+    return (
+      <textarea
+        value={props.value}
+        className="w-full p-2 border-2 border-gray-100 h-64
+hover:border-gray-300 focus:border-gray-500 rounded-lg my-2 outline-none
+transition-all"
+        onChange={(e) => props.onChange(e.target.value)}
+      />
+    )
+  }
+  return (
+    <input
+      type={props?.type ?? "text"}
+      className="w-full p-2 border-2 border-gray-100
+hover:border-gray-300 focus:border-gray-500 rounded-lg my-2 outline-none
+transition-all"
+      value={props.value}
+      onChange={(e) => props.onChange(e.target.value)}
+    />
+  )
+}
+
+export default TextInput
